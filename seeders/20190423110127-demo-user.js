0060c388925eec9a66595c56bb30a044cac540df
jalvarez <jalvarez@clever-global.com>
2019-04-23 14:48:02 +0200

    categories model created

--- seeders/20190423110127-demo-user.js
+++ seeders/20190423110127-demo-user.js
@@ -2,16 +2,6 @@
 
 module.exports = {
     up: (queryInterface, Sequelize) => {
-        /*
-          Add altering commands here.
-          Return a promise to correctly handle asynchronicity.
-
-          Example:
-          return queryInterface.bulkInsert('Users', [{
-            name: 'John Doe',
-            isBetaMember: false
-          }], {});
-        */
         return queryInterface.bulkInsert('Users', [{
             firstName: 'John',
             lastName: 'Cobra',
@@ -21,13 +11,6 @@ module.exports = {
     },
 
     down: (queryInterface, Sequelize) => {
-        /*
-          Add reverting commands here.
-          Return a promise to correctly handle asynchronicity.
-
-          Example:
-          return queryInterface.bulkDelete('Users', null, {});
-        */
         return queryInterface.bulkDelete('Users', null, {});
     }
 };
--- seeders/20190423123921-demo-type.js
+++ seeders/20190423123921-demo-type.js
@@ -2,16 +2,6 @@
 
 module.exports = {
     up: (queryInterface, Sequelize) => {
-        /*
-          Add altering commands here.
-          Return a promise to correctly handle asynchronicity.
-
-          Example:
-          return queryInterface.bulkInsert('People', [{
-            name: 'John Doe',
-            isBetaMember: false
-          }], {});
-        */
         return queryInterface.bulkInsert('Types', [{
             id: 'CRD',
             name: 'Cerdo',
@@ -27,12 +17,6 @@ module.exports = {
     },
 
     down: (queryInterface, Sequelize) => {
-        /*
-          Add reverting commands here.
-          Return a promise to correctly handle asynchronicity.
-
-          Example:
-          return queryInterface.bulkDelete('People', null, {});
-        */
+        return queryInterface.bulkDelete('Types', null, {});
     }
 };
