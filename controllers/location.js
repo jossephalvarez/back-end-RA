c592ca60c6af7899799762e9207186b6ec4e4288
jalvarez <jalvarez@clever-global.com>
2019-04-25 15:50:38 +0200

    location controllers to create and list

--- controllers/location.js
+++ controllers/location.js
@@ -7,4 +7,10 @@ module.exports = {
             .then(location => res.status(201).send(location))
             .catch(error => res.status(400).send(error));
     },
+    list(req, res) {
+        return Location
+            .findAll()
+            .then((locations) => res.status(200).send(locations))
+            .catch((error) => { res.status(400).send(error); });
+    },
 };
--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,13 @@ var router = express.Router();
 const locationController = require('../controllers').location;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
 
 /* Location Router */
-router.get('/api/company', locationController.list);
-router.get('/api/company/:id', companyController.getById);
-router.post('/api/company', companyController.add);
-router.put('/api/company/:id', companyController.update);
-router.delete('/api/company/:id', companyController.delete);
+router.get('/api/location', locationController.list);
+router.post('/api/location', locationController.add);
 
 /* Advance Router */
 
